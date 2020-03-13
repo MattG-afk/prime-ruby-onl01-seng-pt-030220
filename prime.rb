a19b0ee1bae07dc9de604a2e268326eebbc511c3
Matt Gordon <bigmattsize21@gmail.com>
2020-03-13 21:40:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(num)
-  num.each do |x|
+  num.split.each do |x|
  if !num.interger?
   false
  elsif num < 2 
   false
  elsif num == 2
   true
- elsif num.split.each {|x| return false if num % 0}
+ elsif num.each do {|x| return false if num % 0}
  end
 end
 end
