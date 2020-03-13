486a426fe16ce7c670ceed8fb4d04628f1ea2f5f
Matt Gordon <bigmattsize21@gmail.com>
2020-03-13 21:29:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   number = 2
   return false if !number.integer?
   return false if number < 2
-  return false if number == -1 
+  return false if number == 
   return TRUE if number == 2 
   (2..-1).each {|int| return false if number % int == 0 }
   true
