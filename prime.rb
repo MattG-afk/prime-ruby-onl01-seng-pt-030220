49032cd70fa05cc5cf83697f749a1b324e61afb1
Matt Gordon <bigmattsize21@gmail.com>
2020-03-13 21:40:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,8 @@
-def prime?(num)
-  num.split.each do |x|
- if !num.interger?
-  false
- elsif num < 2 
-  false
- elsif num == 2
-  true
- elsif num.each do {|x| return false if num % 0}
- end
-end
-end
+def prime?(integer) 
+  i= 2 
+    return false if integer % i == 0 || integer <= 1
+    while i < integer
+        i += 1
+        return true
+    end
+  end
